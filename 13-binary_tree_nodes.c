ddfaec959c7bb7bc46af7b94f55dba6db9bb6f93
sunnywizy <sundayobasi222@gmail.com>
2024-03-01 04:00:34 +0100

    I just added some files to my repository

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - function that returns # of nodes in a tree with children
+ * @tree: tree to check
+ * Return: number of nodes with children
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+	size_t  nodes = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		nodes += ((tree->left || tree->right) ? 1 : 0);
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+		return (nodes);
+	}
+}
